a788cd0a8fa7f0ca7fd6f7dc6ec4f48fabad21e5
rookedsysc <rookedsysc36@gmail.com>
2025-03-11 17:37:48 +0900

    feat: button 생성 및 App.tsx에 Button Render

--- example/react-hello-world/src/App.tsx
+++ example/react-hello-world/src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button />
     </>
   );
 }
---
+++ example/react-hello-world/src/components/Button.tsx
@@ -0,0 +1,5 @@
+const Buttom = () => {
+  return <button>Click me</button>;
+};
+
+export default Buttom;
