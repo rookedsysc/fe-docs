68ca6264ced757c4e68d4891678fd2d6b97f6090
rookedsysc <rookedsysc36@gmail.com>
2025-03-11 17:28:28 +0900

    feat: p tag에 직접 style 속성 주기

--- example/react-hello-world/src/components/Main.tsx
+++ example/react-hello-world/src/components/Main.tsx
@@ -3,7 +3,9 @@ const Main = () => {
   return (
     <div>
       <h1>Main</h1>
-      <p>{user.login ? <div> 로그아웃</div> : <div>로그인</div>}</p>
+      <p style={{ backgroundColor: "red", borderBottom: "1px solid blue" }}>
+        {user.login ? <div> 로그아웃</div> : <div>로그인</div>}
+      </p>
     </div>
   );
 };
