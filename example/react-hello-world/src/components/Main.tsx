d833b932c21fc5f6874d6ea9a45a651fb2a156c7
rookedsysc <rookedsysc36@gmail.com>
2025-03-11 17:23:18 +0900

    feat: 3항 연산자 활용해서 다른 tag 보여주도록 설정

--- example/react-hello-world/src/components/Main.tsx
+++ example/react-hello-world/src/components/Main.tsx
@@ -1,8 +1,9 @@
 const Main = () => {
+  const user = { name: "John Doe", login: false };
   return (
     <div>
       <h1>Main</h1>
-      <p>Content</p>
+      <p>{user.login ? <div> 로그아웃</div> : <div>로그인</div>}</p>
     </div>
   );
 };
