761ca84ece8c4122804ade97d5425b934a36648f
rookedsysc <rookedsysc36@gmail.com>
2025-03-11 17:37:27 +0900

    feat: css와 class를 사용해서 tag에 style 지정

--- example/react-hello-world/src/components/Main.tsx
+++ example/react-hello-world/src/components/Main.tsx
@@ -1,10 +1,16 @@
+import "./Main.css";
+
 const Main = () => {
-  const user = { name: "John Doe", login: false };
+  const user = { name: "John Doe", login: true };
   return (
     <div>
       <h1>Main</h1>
-      <p style={{ backgroundColor: "red", borderBottom: "1px solid blue" }}>
-        {user.login ? <div> 로그아웃</div> : <div>로그인</div>}
+      <p>
+        {user.login ? (
+          <div className="logout"> 로그아웃</div>
+        ) : (
+          <div>로그인</div>
+        )}
       </p>
     </div>
   );
